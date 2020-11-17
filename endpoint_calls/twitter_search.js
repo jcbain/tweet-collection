b51f8c5b62aaba171bb8a5de2cfb5e2410fe52b1
jcbain <jamescbain@gmail.com>
2020-11-16 23:16:37 -0700

    compartmentalize axios request and store data in database

--- endpoint_calls/twitter_search.js
+++ endpoint_calls/twitter_search.js
@@ -6,25 +6,34 @@ const bearer = process.env.TWITTER_BEARER_TOKEN
 const headers = { headers: { AUTHORIZATION : `Bearer ${bearer}`} }
 const query = 'INSERT INTO tweets(id, tweet_text) VALUES($1, $2)'
 
-// fetch tweets from a certain endpoint
-const fetchTwitterData = (callback) => {
-    axios.get('https://api.twitter.com/2/tweets/search/recent?query=from:petersagal&tweet.fields=referenced_tweets,author_id,created_at', headers)
-       .then(res => {
-         callback(res.data);
-    })
-    .catch(err => `error: ${err}`)
 
+const fetchTwitterData = (query, callback) => {
+    axios.get(`https://api.twitter.com/2/tweets/search/recent?query=${query}&tweet.fields=id,text,author_id,created_at,conversation_id`, headers)
+        .then(response => {
+            callback(response.data)
+        })
+        .catch(err => `an error occurred at fetchTwitterData: ${err}`)
 }
 
-fetchTwitterData(res => {
-    res.data.forEach(element => {
-        console.log(`tweet: ${element.id} text: ${element.text} author: ${element.author_id} created_at: ${element.created_at}`)
-        if(element.referenced_tweets){
-            console.log(element.referenced_tweets)
-        }
+module.exports = { fetchTwitterData }
+// fetch tweets from a certain endpoint
+// const fetchTwitterData = (callback) => {
+//     axios.get('https://api.twitter.com/2/tweets/search/recent?query=from:petersagal&tweet.fields=referenced_tweets,author_id,created_at', headers)
+//        .then(res => {
+//          callback(res.data);
+//     })
+//     .catch(err => `error: ${err}`)
+// }
+
+// fetchTwitterData(res => {
+//     res.data.forEach(element => {
+//         console.log(`tweet: ${element.id} text: ${element.text} author: ${element.author_id} created_at: ${element.created_at}`)
+//         if(element.referenced_tweets){
+//             console.log(element.referenced_tweets)
+//         }
         
-    });
-})
+//     });
+// })
 
 // invoke fetch tweets and save the results to the database
 // fetchTwitterData((res) => {
--- routes/tweet_router.js
+++ routes/tweet_router.js
@@ -2,32 +2,57 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors')
 const pool = require('../database/db');
-
-
-// const origin  = 'http://localhost:3000';
+const { fetchTwitterData } = require('../endpoint_calls/twitter_search');
 
 const corsOptions = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200
 }
 
-pool.connect( () => console.log("connected to thd db"))
+
 
 const getAllJobs = async () => {
+    const client = await pool.connect(console.log("🔌 Connected to the db"))
     const query = "SELECT * FROM jobs";
-    return await pool.query(query)
+    return await client.query(query)
         .then(resp => resp.rows)
         .catch(err => `There was an error thrown at getAllJobs(): ${err}`)
 }
 
+
 router.get('/', cors(corsOptions), (req, res) => {
+
     getAllJobs()
         .then(resp => res.json(resp))
         .catch(err => console.log(err.stack))
 })
 
 router.post('/:id', cors(corsOptions), (req, res) => {
-    console.log(req.body)
+    const { query, jobId } = req.body;
+    const queryString = 'INSERT INTO tweets(id, tweet_text, author_id, created_at, conversation_id, job_id) VALUES($1, $2, $3, $4, $5, $6)'
+    console.log(query)
+    fetchTwitterData(query, resp => {
+            pool.connect(( err, client, done ) => {
+                if (err) throw `error: ${err}`;
+                try {
+                    console.log("🔌 Connected to the db");
+                    resp.data.forEach( (row, i) => {
+                        if (i === 0) res.json(row)
+                        client.query(queryString, [row.id, row.text, row.author_id, row.created_at, row.conversation_id, jobId], (err, response) => {
+                            if (err) {
+                                console.log(err.stack);
+                            } else {
+                                console.log('inserted row')
+                            }
+                        })
+                    })
+
+                } finally {
+                    done()
+                }
+            });
+        })
+    
 })
 
 module.exports = router;
