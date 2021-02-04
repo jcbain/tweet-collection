1f56da2bc4d7ed225badd9b2c201e44f5e106c79
jcbain <jamescbain@gmail.com>
2021-02-03 17:44:18 -0700

    update migrations with referenced tweets

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
     connection: {
         user: process.env.USER,
         host: process.env.DB_HOST || 'localhost',
-        database: 'twitter',
+        database: 'twitter-dev',
         password: process.env.DB_PASS,
         port: 5432
     },
@@ -22,37 +22,22 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: {
+      user: process.env.USER,
+      host: process.env.DB_HOST || 'localhost',
+      database: 'twitter',
+      password: process.env.DB_PASS,
+      port: 5432
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 
 };
