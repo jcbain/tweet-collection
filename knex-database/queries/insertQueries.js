76e8651e805be897635968765005fc8162482dc9
jcbain <jamescbain@gmail.com>
2021-02-04 15:58:46 -0700

    conditionally get coordinates if geo and coordinates both exist

--- knex-database/queries/insertQueries.js
+++ knex-database/queries/insertQueries.js
@@ -35,6 +35,7 @@ const mappedKeys =  {
     text: 'tweet_text',
     author_id: 'author_id',
     created_at: 'created_at',
+    butts: 'super_butts'
 };
 
 const list = [
@@ -42,21 +43,64 @@ const list = [
         id: '123',
         text: 'some text',
         author_id: 'akdjafads',
-        created_at: new Date()
+        created_at: new Date(),
+        geo: {
+            place_id: "2595040ad5860917",
+            coordinates: {
+                type: "Point",
+                coordinates: [
+                    -111.9927,
+                    41.17666
+                ]
+            }
+        }
     },
     {
         id: '124',
         text: 'some text again',
         author_id: 'akdjafads',
-        created_at: new Date()
+        created_at: new Date(),
+        geo: {
+            place_id: "2595040ad5860917",
+        }
     }
 ]
+
+const obj = {
+    name: 'james',
+    geo: {
+        place_id: "2595040ad5860917",
+        coordinates: {
+            type: "Point",
+            coordinates: [
+                -111.9927,
+                41.17666
+            ]
+        }
+    }
+}
+
+const obj2 = {
+    name: 'james',
+    geo: {
+        place_id : "2595040ad5860917"
+    }
+}
+
+const { geo: { coordinates: { coordinates = null } = {coordinates: null} } = { geo: null }} = obj2;
+
+console.log(coordinates)
 
 
 const insertIntoTweets = async (data) => {
     let newData = [];
     for( const row of data ){
         const renamedObj = await renameKeys(row, mappedKeys);
+        const { geo: { coordinates: { coordinates = null } = {coordinates: null} } = { geo: null }} = row;
+        if(coordinates){
+            renamedObj.lng = coordinates[0];
+            renamedObj.lat = coordinates[1];
+        }
         newData.push(renamedObj)
     }
     return newData
